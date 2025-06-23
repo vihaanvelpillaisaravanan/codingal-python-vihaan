711453c4d5500ea500bbbd835d7758b4d466cf2c
vihaanvelpillaisaravanan <vihaansaran2012@gmail.com>
2025-06-23 13:35:46 +0800

    Lesson2 done

---
+++ Lesson2/activity1.py
@@ -0,0 +1,15 @@
+# prints statement
+print("welcome to the world of programming!")
+
+#print integers
+print(500)
+
+#print line break
+print("i want a line break\n")
+
+#print mutiple
+print("what is the squre of 9",9*9)
+
+#print mutiple print in a line
+print("my name is",end=" ")
+print("vihaan")
---
+++ Lesson2/activity2.py
@@ -0,0 +1,11 @@
+# integers
+number = 3.4
+name = "vihaan"
+
+print(number)
+print(name)
+
+
+# asking user input
+school = input("what is your school name?: ")
+print("your school name is",school,". your school is a great institution")
