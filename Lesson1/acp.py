77ef62e613773e4c6a7d18f2bf451cd8f33c52d9
vihaanvelpillaisaravanan <vihaansaran2012@gmail.com>
2025-06-27 11:59:32 +0800

    acp done

--- Lesson1/acp.py
+++ Lesson1/acp.py
@@ -1,9 +1,3 @@
-print("Hello, my name is Vihaan and I am studying in Grade 7.")
-print("I think Python is very simple and easy to learn. I would rather learn Python than English!")
-
-their_opinion = input("Do you agree? (yes/no): ")
-
-if their_opinion == "yes":
-    print("Yeah! Python is awesome!")
-else:
-    print("Thanks for your opinion!")
+print("my name is vihaan")
+print("i am studying 7th AKA sec1 in singapore")
+print("i think python is a very simple a easy language to learn and would rather learn this than english")
