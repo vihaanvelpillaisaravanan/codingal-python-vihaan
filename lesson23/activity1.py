1a9f849927b20507d99a80da926bbed0be1eed5e
vihaanvelpillaisaravanan <vihaansaran2012@gmail.com>
2026-04-14 19:25:31 +0800

    acp done

---
+++ lesson23/activity1.py
@@ -0,0 +1,18 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1,0,"hello",(1,2,3)}
+print(my_set)
+
+my_set = (1,2,3,4,8,7)
+print(my_set)
+
+my_set = set([1,2,3,1])
+print(my_set)
+
+num_set = set([0,1,4,6,7])
+print("oringinal set")
+print(num_set)
+num_set.pop()
+print("after removing the first emement from the sailset:")
+print(num_set,"\n")
---
+++ lesson23/activity2.py
@@ -0,0 +1,8 @@
+setx = {"green", "yellow"}
+sety = {"blue", "yelow"}
+print("oringinal set element")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
